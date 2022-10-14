81a5c91314b0774a0cc872a11e8db7de45f20ed5
wcx <1121275003@qq.com>
2022-10-14 16:40:18 +0800

    fix: now the delta and response will be vertically listed

--- PUFs.py
+++ PUFs.py
@@ -5,11 +5,10 @@ from math import sqrt
 
 class APUF:
     def transform(challenge):
-        challenge = torch.cat((challenge, torch.tensor([1])), 0)
+        challenge = torch.cat((challenge, torch.ones(size=(1,))), 0)
         for i in range(challenge.shape[0] - 2, -1, -1):
             challenge[i] = challenge[i + 1] * (1 - 2 * challenge[i])
         return challenge
-
 
     def __init__(self, challenge, weight, transformed=False):
         self.challenge = challenge
@@ -19,7 +18,7 @@ class APUF:
         assert self.challenge.shape[-1] == self.weight.shape[-1], "Shape Error"
             
     def forward(self):
-        ans = torch.sum(self.challenge * self.weight)
+        ans = torch.sum(self.challenge * self.weight, dim=0, keepdim=True)
         return ans
     
     def getResponse(self):
@@ -39,12 +38,11 @@ class XORAPUF(APUF):
         self.challenge = challenge
         self.weight = weight
         if not transformed:
-            for i in range(self.challenge.shape[0]): # expend line by line will make the tensor not rectangular
-                self.challenge[i] = XORAPUF.transform(self.challenge[i])
+            self.challenge = XORAPUF.transform(self.challenge)
         assert self.challenge.shape[-1] == self.weight.shape[-1], "Shape Error"
         
     def forward(self):
-        ans = torch.sum(self.challenge * self.weight, dim=1)
+        ans = torch.sum(self.challenge * self.weight, dim=1, keepdim=True)
         return ans
     
     def getResponse(self):
@@ -56,18 +54,15 @@ class XORAPUF(APUF):
         return response
     
     def randomSample(n, length):
-        challenge = torch.tensor([[randint(0, 1) for _ in range(length)] for _ in range(n)])
+        challenge = torch.tensor([randint(0, 1) for _ in range(length)])
         weight = torch.normal(0, sqrt(0.05), size=(n, length + 1))
         return XORAPUF(challenge, weight, transformed=False)
             
-    
-        
-    
-
 
 APUFSample = APUF.randomSample(128)
-print(APUFSample.forward(), APUFSample.getResponse())
+print(APUFSample.forward())
+print(APUFSample.getResponse())
 
 XORAPUFSample = XORAPUF.randomSample(2, 128)
-print(XORAPUFSample.forward(), XORAPUFSample.getResponse())
-
+print(XORAPUFSample.forward())
+print(XORAPUFSample.getResponse())
